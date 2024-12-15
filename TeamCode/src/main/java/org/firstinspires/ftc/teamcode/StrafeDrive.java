a6fe781a286f2c6c84007f3cedd2d075a6468607
kenna26 <90514808+kenna26@users.noreply.github.com>
2024-12-15 14:36:50 -0500

    initial thoughts
    missing derivaite of sigmoid function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StrafeDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StrafeDrive.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class StrafeDrive {
 
@@ -9,6 +10,13 @@ public class StrafeDrive {
     private DcMotor lf;
     private DcMotor lb;
 
+    //test for this value after any major changes to the robot
+    //limit is found when robot start to slip/skid when acceleration
+    private double maxAcceleration = 5; //measure in some unit
+
+    //smallest sigmoid curve that does not go over max acceleration (at x=0)
+    private double horizontalStretchSigmoid;
+
     private double speed = 0.5;
 
     public StrafeDrive(DcMotor rf, DcMotor rb, DcMotor lf, DcMotor lb) {
@@ -16,6 +24,8 @@ public class StrafeDrive {
         this.rb = rb;
         this.lf = lf;
         this.lb =lb;
+
+        horizontalStretchSigmoid = calculateSigmoidHorizontalStretch(maxAcceleration);
     }
 
 
@@ -46,10 +56,10 @@ public class StrafeDrive {
     }
 
     public void stop () {
-        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rf.setPower(0);
+        rb.setPower(0);
+        lf.setPower(0);
+        lb.setPower(0);
     }
 
     private void setEachPower (double rfp, double rbp, double lfp, double lbp) {
@@ -64,5 +74,58 @@ public class StrafeDrive {
     }
 
     public double getSpeed() { return speed; }
+
+    //Sigmoid driving
+
+    /**
+     * this is not an async function it will steal your thread
+     * @param maxPower max power of the wheels
+     * @param totalTime total time for movement
+     */
+    private void verticalSigmoid(double maxPower, int totalTime) {
+        ElapsedTime timer = new ElapsedTime();
+
+        //domain of sigmoid function for acceleration is [-1,1] * maxPower
+        //so total domain is 2*maxPower
+        //the domain is the time used for acceleration
+        double sigmoidDomain = 2 * maxPower;
+
+        //this is the amount of time the wheels are moving at max power
+        //this is the total time - (2* acceleration time)
+        //acceleration time is the domain of the sigmoid
+        double maxPowerTime = totalTime - (2*sigmoidDomain);
+
+        //acceleration period
+        timer.reset();
+        while(timer.time() < sigmoidDomain) {
+            vertical(maxPower * (sigmoid(timer.time())));
+        }
+
+        //regular straight motion
+        timer.reset();
+        while(timer.time() < maxPowerTime) {
+            vertical(maxPower);
+        }
+
+        //deceleration period
+        timer.reset();
+        while(timer.time() < sigmoidDomain) {
+            //reflects sigmoid over y axis by negatizing x values
+            vertical(maxPower * sigmoid(-timer.time()));
+        }
+
+        //stop motion
+        stop();
+    }
+
+    private double sigmoid(double time) {
+        double stretchedX = horizontalStretchSigmoid * time;
+        return (Math.exp(stretchedX)/(1+Math.exp(stretchedX)));
+    }
+
+    private double calculateSigmoidHorizontalStretch(double maxAcceleration) {
+        //TODO find the derivative of a sigmoid function as a function of its horizontal stretch component
+        return 5;
+    }
 
 }
