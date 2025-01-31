84eaf1413be9b6336528f44b4420dcc5e2cd52c2
kenna26 <90514808+kenna26@users.noreply.github.com>
2025-01-31 16:39:48 -0500

    c

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmLift/TestArmOpModes/TeleOpArm.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.ArmLift.TestArmOpModes;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.ArmLift.Claw;
+import org.firstinspires.ftc.teamcode.TeleOp;
+
+public class TeleOpArm {
+
+    public TeleOpArm (DcMotor Lift, DcMotor Wrist, Claw claw) {
+        
+    }
+}
